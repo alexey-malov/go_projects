6d54d54c7c8abd6d91634db9488ade0aea497bb9
Alexey Malov <alexey.malov@cpslabs.net>
2017-03-18 01:47:40 +0300

    Теперь количество бутылок пива выводится в виде слов

--- bottles/bottles.go
+++ bottles/bottles.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+	"unicode/utf8"
+)
 
-func fmtBottles(i int) string {
+func fmtBottles(i uint) string {
 	if i == 0 {
 		return "бутылок"
 	} else if i == 1 {
@@ -14,21 +18,97 @@ func fmtBottles(i int) string {
 	} else {
 		return fmtBottles(i % 10)
 	}
+}
 
+func n2s99(i uint) string {
+	if i > 99 {
+		panic(fmt.Sprintf("Argument %d is out of range:[0..99]", i))
+	}
+	units0to19 := [20]string{
+		"ноль", "одна", "две", "три", "четыре",
+		"пять", "шесть", "семь", "восемь", "девять",
+		"десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать",
+		"пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать",
+	}
+
+	if i < 20 {
+		return units0to19[i]
+	}
+
+	tens20to90 := [8]string{
+		"двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто",
+	}
+	tens := (i / 10)
+
+	tensStr := tens20to90[tens-2]
+
+	units := i % 10
+	unitsStr := units0to19[units]
+	if units == 0 {
+		unitsStr = ""
+	}
+
+	result := tensStr
+	if unitsStr != "" {
+		result = fmt.Sprintf("%s %s", tensStr, unitsStr)
+	}
+
+	return result
+}
+
+var hundreds0to900 = [10]string{
+	"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот",
+}
+
+func n2s999(i uint) string {
+	if i > 999 {
+		panic(fmt.Sprintf("Argument %d is out of range:[0..999]", i))
+	}
+
+	hundreds := i / 100
+
+	result := ""
+	if units := i % 100; hundreds == 0 || units != 0 {
+		result = n2s99(units)
+	}
+
+	hundredsStr := hundreds0to900[hundreds]
+
+	if hundredsStr != "" {
+		if result != "" {
+			result = fmt.Sprintf("%s %s", hundredsStr, result)
+		} else {
+			result = hundredsStr
+		}
+	}
+
+	return result
+}
+
+func numericToString(i uint) string {
+	return n2s999(i)
+}
+
+func toTitleCase(s string) string {
+	r, i := utf8.DecodeRuneInString(s)
+	return string(unicode.ToUpper(r)) + s[i:]
 }
 
 func main() {
-	maxBottles := 500
+	// Must be in range [0..999]
+	maxBottles := uint(999)
 	for i := maxBottles; i > 0; i-- {
 		bottles := fmtBottles(i)
-		fmt.Println(i, bottles, "пива на стене")
-		fmt.Println(i, bottles, "пива!")
-		fmt.Println("Возьми одну, пусти по кругу")
-		fmt.Println(i-1, fmtBottles(i-1), "пива на стене!\n")
+		numBottles := toTitleCase(numericToString(i))
+
+		fmt.Println(numBottles, bottles, "пива на стене!")
+		fmt.Println(numBottles, bottles, "пива!")
+		fmt.Println("Возьми одну, пусти по кругу!")
+		fmt.Println(toTitleCase(numericToString(i-1)), fmtBottles(i-1), "пива на стене!\n")
 	}
 
 	fmt.Println("Нет бутылок пива на стене!")
 	fmt.Println("Нет бутылок пива!")
 	fmt.Println("Пойди в магазин и купи ещё,")
-	fmt.Println(maxBottles, "бутылок пива на стене!")
+	fmt.Println(toTitleCase(numericToString(maxBottles)), "бутылок пива на стене!")
 }
