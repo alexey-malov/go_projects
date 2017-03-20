e7a535d29728a55566493511654f698259e238de
Alexey Malov <alexey.malov@cpslabs.net>
2017-03-20 23:15:11 +0300

    добавлен пример font

---
+++ workshop02/font/font.go
@@ -0,0 +1,49 @@
+package font
+
+import "fmt"
+
+type Font struct {
+	family string
+	size int
+}
+
+const MIN_SIZE = 5
+const MAX_SIZE = 144
+
+func clampSize(size int) int {
+	if size < MIN_SIZE {
+		size = MIN_SIZE
+	} else if size > MAX_SIZE {
+		size = MAX_SIZE
+	}
+	return size
+}
+
+func New(family string, size int)*Font {
+	if family == "" {
+		panic("Invalid family name")
+	}
+	return &Font{family, clampSize(size)}
+}
+
+func (f *Font) Family() string {
+	return f.family
+}
+
+func (f *Font) Size() int {
+	return f.size
+}
+
+func (f *Font) String() string {
+	return fmt.Sprintf(`{font-family: "%s"; font-size: %dpt;}`, f.family, f.size)
+}
+
+func (f *Font) SetSize(size int) {
+	f.size = clampSize(size)
+}
+
+func (f *Font) SetFamily(family string) {
+	if family != "" {
+		f.family = family
+	}
+}
---
+++ workshop02/font/font_test.go
@@ -0,0 +1,105 @@
+package font_test
+
+import (
+	"testing"
+	"fmt"
+	"github.com/alexey-malov/go_projects/workshop02/font"
+)
+
+func TestFontConstruction(t *testing.T) {
+	expectSuccessfulConstruction("f", 5, t)
+	expectSuccessfulConstruction("f", 144, t)
+	expectFontConstruction("fnt", 4, 5, t)
+	expectFontConstruction("fnt", 145, 144, t)
+}
+
+func TestFont(t *testing.T) {
+	bodyFont := font.New("Nimbus Sans", 10)
+	titleFont := font.New("serif", 11)
+	f1(bodyFont, titleFont, t)
+}
+
+func TestFont_SetSize(t *testing.T) {
+	f := font.New("Family", 10)
+
+	f.SetSize(font.MIN_SIZE)
+	if f.Size() != 5 {
+		t.Fatal("Failed to set font size to 5")
+	}
+
+	f.SetSize(font.MIN_SIZE - 1)
+	if f.Size() != font.MIN_SIZE {
+		t.Fatal("Font size must be set to font.MIN_SIZE")
+	}
+
+	f.SetSize(font.MAX_SIZE)
+	if f.Size() != font.MAX_SIZE {
+		t.Fatal("Failed to set max font size")
+	}
+
+	f.SetSize(font.MAX_SIZE + 1)
+	if f.Size() != font.MAX_SIZE {
+		t.Fatal("Font size must be set to font.MAX_SIZE")
+	}
+}
+
+func expectSuccessfulConstruction(family string, size int, t *testing.T) {
+	expectFontConstruction(family, size, size, t)
+}
+
+func expectFontConstruction(family string, size, expectedSize int, t *testing.T) {
+	fnt := font.New(family, size)
+
+	gotFamily := fnt.Family()
+	if fnt.Family() != family {
+		t.Errorf("Expected font.Family() to be equal to '%s' but got '%s'", family, gotFamily)
+		return
+	}
+
+	gotSize := fnt.Size()
+	if fnt.Size() != expectedSize {
+		t.Errorf("Expected font.Size() to be equal to '%d' but got '%d'", expectedSize, gotSize)
+		return
+	}
+}
+
+func f1(bodyFont, titleFont *font.Font, t *testing.T) {
+	if bodyFont.String() !=
+		`{font-family: "Nimbus Sans"; font-size: 10pt;}` {
+		t.Fatal("#1 bodyFont invalid CSS")
+	}
+	if bodyFont.Size() != 10 || bodyFont.Family() != "Nimbus Sans" {
+		t.Fatal("#2 bodyFont invalid attributes")
+	}
+	bodyFont.SetSize(12)
+	if bodyFont.Size() != 12 || bodyFont.Family() != "Nimbus Sans" {
+		t.Fatal("#3 bodyFont invalid attributes")
+	}
+	if bodyFont.String() !=
+		`{font-family: "Nimbus Sans"; font-size: 12pt;}` {
+		t.Fatal("#4 bodyFont invalid CSS")
+	}
+	bodyFont.SetFamily("")
+	if bodyFont.Size() != 12 || bodyFont.Family() != "Nimbus Sans" {
+		t.Fatal("#5 bodyFont invalid attributes")
+	}
+
+	if titleFont.String() != `{font-family: "serif"; font-size: 11pt;}` {
+		t.Fatal("#6 titleFont invalid CSS")
+	}
+	if titleFont.Size() != 11 || titleFont.Family() != "serif" {
+		t.Fatal("#7 titleFont invalid attributes")
+	}
+	titleFont.SetFamily("Helvetica")
+	titleFont.SetSize(20)
+	if titleFont.Size() != 20 || titleFont.Family() != "Helvetica" {
+		t.Fatal("#8 titleFont invalid attributes")
+	}
+
+	f2(bodyFont, titleFont)
+}
+
+func f2(bodyFont, titleFont *font.Font) {
+	fmt.Println(bodyFont)
+	fmt.Println(titleFont)
+}
